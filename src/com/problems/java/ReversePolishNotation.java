f3e0d0edca26e183b95aa166388dac99ff1128c5
Arpit <jindalarpit@gmail.com>
2022-06-05 23:14:45 +0530

    updated formatting

--- src/com/problems/java/ReversePolishNotation.java
+++ src/com/problems/java/ReversePolishNotation.java
@@ -6,7 +6,8 @@ import java.util.Stack;
 Evaluate the value of an arithmetic expression in Reverse Polish Notation.
 Valid operators are +, -, *, and /. Each operand may be an integer or another expression.
 Note that division between two integers should truncate toward zero.
-It is guaranteed that the given RPN expression is always valid. That means the expression would always evaluate to a result, and there will not be any division by zero operation.
+It is guaranteed that the given RPN expression is always valid.
+That means the expression would always evaluate to a result, and there will not be any division by zero operation.
 
 Example 1:
 Input: tokens = ["2","1","+","3","*"]
@@ -28,6 +29,7 @@ Explanation: ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
 = (0 + 17) + 5
 = 17 + 5
 = 22
+
  */
 public class ReversePolishNotation {
 
